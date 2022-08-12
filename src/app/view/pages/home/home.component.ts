a38390ec2229263ce54adc8377de8e5faf688c37
Jhonatan Izidoro <jhonatan.antunes@meep.com.br>
2022-08-12 14:11:57 -0300

    criando modal customizado para aplicação e adicionando ele na pagina home

--- src/app/view/pages/home/home.component.ts
+++ src/app/view/pages/home/home.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../modal';
 @Component({
   templateUrl: 'home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  bodyText: string | undefined;
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+  openModal(id: string) {
+    console.log("teste");
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
 }
--- src/app/view/pages/home/home.module.ts
+++ src/app/view/pages/home/home.module.ts
@@ -7,9 +7,10 @@ import { NgxMaskModule } from 'ngx-mask';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing';
 import { throwIfAlreadyLoaded } from '../../../services/guards/module-import.guard';
+import {ModalModule} from "../modal";
 
 @NgModule({
-  imports: [HomeRoutingModule],
+  imports: [HomeRoutingModule, ModalModule, FormsModule],
   exports: [],
   declarations: [HomeComponent],
   providers: [],
