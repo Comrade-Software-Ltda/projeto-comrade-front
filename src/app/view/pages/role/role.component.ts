2f3edf963610847ccb8c1bc2995363c426fd08f7
edsonrdl <edson@EDSONRDL>
2022-09-15 11:44:57 -0300

    feat:Adicionado CRUD ao Role.component.ts

--- src/app/view/pages/role/role.component.ts
+++ src/app/view/pages/role/role.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { GetAllRoleUsecase } from '../../../core/usecases/role/get-all-role.usecase';
-import { RoleModel } from '../../../core/models/role.model';
+import {RoleModel,} from '../../../core/models/role.model';
 import { PageResultModel } from '../../../core/utils/responses/page-result.model';
+import { PostRoleUsecase } from 'src/app/core/usecases/role/post-role.usecase';
+import { DeleteRoleUsercase } from 'src/app/core/usecases/role/delete-role.usercase';
+import { GetRoleByIdUsecase } from 'src/app/core/usecases/role/get-role-by-id.usecase';
+import { PutRoleUsecase } from 'src/app/core/usecases/role/put-role.usecase';
+import { SingleResultModel } from 'src/app/core/utils/responses/single-result.model';
+
 
 @Component({
   selector: 'app-role',
@@ -11,16 +17,48 @@ import { PageResultModel } from '../../../core/utils/responses/page-result.model
 })
 export class RoleComponent implements OnInit {
   dataSource!: RoleModel[];
-  constructor(private getAllRole: GetAllRoleUsecase) {}
+
+  constructor(
+    private deleteRoleUsercase: DeleteRoleUsercase,
+    private getAllRoleUsecase: GetAllRoleUsecase,
+    private getRoleByIdUsecase: GetRoleByIdUsecase,
+    private postRoleUsecase: PostRoleUsecase,
+    private putRoleUsecase: PutRoleUsecase
+  ) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
-
-  getAll(){
-    this.getAllRole
+  getAll(): void {
+    this.getAllRoleUsecase
       .execute({ pageSize: 20, pageNumber: 1 })
       .subscribe((grid: PageResultModel<RoleModel>) => {
-        this.dataSource = grid.data!;
+        this.dataSource = grid.data ?? [];
       });
+  }
+  getById(): void {
+    this.getRoleByIdUsecase
+      .execute('id')
+      .subscribe((model: SingleResultModel<RoleModel>) => {
+        console.log(model);
+      });
+  }
+  create(e: any): void {
+    console.log(e);
+    const model = e.data as RoleModel;
+    this.postRoleUsecase.execute(model).subscribe();
+    console.log(model);
+  }
+  
+  edit(e: any): void {
+    console.log(e);
+    const model = { ...e.oldData, ...e.newData } as RoleModel;
+    this.putRoleUsecase.execute(model).subscribe();
+  }
+  delete(e: any): void {
+    const model = e.data as RoleModel;
+    if (model.id) {
+      this.deleteRoleUsercase.execute(model.id).subscribe();
+    }
   }
 }
