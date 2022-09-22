ece6b01cfdf8cb1d51448d5a34769fe93398684b
abraaoribeiro23 <abraaoribeiroo23@gmail.com>
2022-09-22 20:29:59 -0300

    feat: field to select icons to system menu

--- src/app/view/pages/system-menu/system-menu.component.ts
+++ src/app/view/pages/system-menu/system-menu.component.ts
@@ -1,11 +1,11 @@
-import { Component, Directive, Injectable, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageResultModel } from 'src/app/core/utils/responses/page-result.model';
 import { SystemMenuModel } from 'src/app/core/models/system-menu.model';
 import { DeleteSystemMenuUsecase } from 'src/app/core/usecases/system-menu/delete-system-menu.usecase';
 import { GetAllSystemMenuUsecase } from 'src/app/core/usecases/system-menu/get-all-system-menu.usecase';
 import { EditSystemMenuUsecase } from 'src/app/core/usecases/system-menu/edit-system-menu.usecase';
 import { CreateSystemMenuUsecase } from 'src/app/core/usecases/system-menu/create-system-menu.usecase';
-
+import { MenuIcons } from 'src/assets/icons-menu';
 @Component({
   selector: 'app-system-menu',
   templateUrl: 'system-menu.component.html',
@@ -14,6 +14,14 @@ import { CreateSystemMenuUsecase } from 'src/app/core/usecases/system-menu/creat
 })
 export class SystemMenuComponent implements OnInit {
   dataSource!: SystemMenuModel[];
+  systemMenuSelected?: SystemMenuModel;
+  menuIcons: string[] = MenuIcons;
+
+  searchModeOption = 'contains';
+  searchTimeoutOption = 200;
+  minSearchLengthOption = 0;
+  showDataBeforeSearchOption = false;
+
   constructor(
     private getAllSystemMenu: GetAllSystemMenuUsecase,
     private createSystemMenu: CreateSystemMenuUsecase,
@@ -35,6 +43,10 @@ export class SystemMenuComponent implements OnInit {
 
   beforeSave(e: any): void {
     e.data.registerDate = new Date();
+  }
+
+  onEditingStart(e: any): void {
+    this.systemMenuSelected = e.data;
   }
 
   save(e: any): void {
--- src/app/view/pages/system-menu/system-menu.module.ts
+++ src/app/view/pages/system-menu/system-menu.module.ts
@@ -1,11 +1,26 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { DxDataGridModule, DxFormModule } from 'devextreme-angular';
+import {
+  DxDataGridModule,
+  DxFormModule,
+  DxSelectBoxModule,
+  DxTextBoxModule,
+  DxTreeListModule,
+} from 'devextreme-angular';
 import { SystemMenuRoutingModule } from './system-menu.routing';
 import { throwIfAlreadyLoaded } from '../../../services/guards/module-import.guard';
 import { SystemMenuComponent } from './system-menu.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [DxDataGridModule, DxFormModule, SystemMenuRoutingModule],
+  imports: [
+    CommonModule,
+    DxDataGridModule,
+    DxSelectBoxModule,
+    DxTreeListModule,
+    DxFormModule,
+    DxTextBoxModule,
+    SystemMenuRoutingModule,
+  ],
   exports: [],
   declarations: [SystemMenuComponent],
   providers: [],
